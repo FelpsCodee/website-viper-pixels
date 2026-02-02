fda3454650ac00e1cd2afb35ee3072d9f6cb3901
FelpsCodee <felipecorreia781@gmail.com>
2026-02-01 21:28:33 -0300

    adicionando filtro de lado ataque ou defesa

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,46 @@
 const express = require('express');
 const fs = require('fs');
-const patch = require('path');
+const path = require('path');
 const app = express();
 
-app.use(express.static(patch.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.get('/api/pixels', (req, res) => {
-    fs.readFile('./data.json','utf8', (err, data) => {
+    const caminhoDoArquivo = path.join(__dirname, 'data.json');
+
+    fs.readFile(caminhoDoArquivo, 'utf8', (err, data) => {
         if (err) {
             return res.status(500).json({ erro: "Não consegui ler o arquivo" });
-
-
         }
-        let ListaDePixels = JSON.parse(data);
 
-        const { mapa, categoria } = req.query;
+        let ListaDePixels = JSON.parse(data);
+        const { mapa, categoria, lado } = req.query;
 
         if (mapa) {
             ListaDePixels = ListaDePixels.filter(p => p.mapa.toLowerCase() === mapa.toLowerCase());
         }
-        if (categoria) {
-            ListaDePixels = ListaDePixels.filter(p => p.categoria.toLowerCase() === categoria.toLowerCase());
+        
+        if (lado) {
+            ListaDePixels = ListaDePixels.filter(p => p.lado && p.lado.toLowerCase() === lado.toLowerCase());
         }
 
-        res.json(ListaDePixels)
+        if (categoria) { 
+            ListaDePixels = ListaDePixels.filter(p => p.categoria && p.categoria.toLowerCase() === categoria.toLowerCase());
+        }
+
+        res.json(ListaDePixels);
     });
 });
 
-app.use('/uploads', express.static('public/uploads'));
-
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000 http://localhost:3000');
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/admin.html'));
 });
 
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
